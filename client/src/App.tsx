5b2076d2b812038e37f5166be9f3cdca5cbdbef6
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-05 15:45:48 +0300

    chore: Update App component to include routing for authentication and dashboard pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "./pages/auth";
+import Dashboard from "./pages/dashboard";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard/>} />
+          <Route path="/sign" element={<Auth/>} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
