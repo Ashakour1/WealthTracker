b9ed0fff9b57b66ddb8d06735cbf82618b62e474
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-05 15:45:38 +0300

    feat: Add authentication page component

---
+++ client/src/pages/auth/index.tsx
@@ -0,0 +1,15 @@
+import {
+  SignIn,
+  SignUp,
+  SignInButton,
+  SignOutButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
+const Auth = () => {
+  return <div>
+    
+  </div>;
+};
+
+export default Auth;
