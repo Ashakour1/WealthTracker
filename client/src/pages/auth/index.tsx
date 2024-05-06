891bd670dc87f03c6750afc7b6296ea0c294917b
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-06 22:05:31 +0300

    chore: Update Auth page to use Clerk's SignedIn and SignedOut components

--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,15 +1,23 @@
 import {
-  SignIn,
-  SignUp,
+  SignedIn,
+  SignedOut,
   SignInButton,
-  SignOutButton,
+  SignUpButton,
   UserButton,
 } from "@clerk/clerk-react";
 
 const Auth = () => {
-  return <div>
-    
-  </div>;
+  return (
+    <div className="sign-in-container">
+      <SignedOut>
+        <SignUpButton mode="modal"/>
+        <SignInButton mode="modal"/>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default Auth;
